40a32b5c59c9e33fb6c80fb376196a8eadf97b15
angelocarly <angelo.carly@hotmail.com>
2019-08-20 01:39:41 +0200

    Add physical device

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <cstring>
 #include <optional>
 
+#include "renderer.h"
+
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
@@ -253,21 +255,25 @@ private:
         uint32_t deviceCount = 0;
         vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 
-        if (deviceCount == 0) {
+        if (deviceCount == 0)
+        {
             throw std::runtime_error("failed to find GPUs with Vulkan support!");
         }
 
         std::vector<VkPhysicalDevice> devices(deviceCount);
         vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
-        for (const auto& device : devices) {
-            if (isDeviceSuitable(device)) {
+        for (const auto &device : devices)
+        {
+            if (isDeviceSuitable(device))
+            {
                 physicalDevice = device;
                 break;
             }
         }
 
-        if (physicalDevice == VK_NULL_HANDLE) {
+        if (physicalDevice == VK_NULL_HANDLE)
+        {
             throw std::runtime_error("failed to find a suitable GPU!");
         }
     }
@@ -312,6 +318,7 @@ private:
 int main()
 {
     Application app;
+    Renderer r;
 
     try
     {
