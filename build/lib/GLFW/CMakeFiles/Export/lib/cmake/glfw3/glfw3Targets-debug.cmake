5e7b7159b11da10fa02640765afad9acf10b4bcf
angelocarly <angelo.carly@hotmail.com>
2019-08-23 01:52:47 +0200

    Change cmake source dir to /src

--- build/lib/GLFW/CMakeFiles/Export/lib/cmake/glfw3/glfw3Targets-debug.cmake
+++ build/lib/GLFW/CMakeFiles/Export/lib/cmake/glfw3/glfw3Targets-debug.cmake
@@ -1,15 +1,15 @@
 #----------------------------------------------------------------
-# Generated CMake target import file.
+# Generated CMake target import file for configuration "Debug".
 #----------------------------------------------------------------
 
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "glfw" for configuration ""
-set_property(TARGET glfw APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+# Import target "glfw" for configuration "Debug"
+set_property(TARGET glfw APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(glfw PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "C"
-  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libglfw3.a"
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libglfw3.a"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS glfw )
