ecb20eefab209d3142d6fc7a8a3d4b55dc3c08ef
angelocarly <angelo.carly@protonmail.com>
2021-01-09 22:48:42 +0100

    Add window

---
+++ src/graphics/vks_window.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+namespace vks
+{
+
+    class VksWindow
+    {
+    public:
+        VksWindow(int w, int h, std::string name);
+
+        ~VksWindow();
+
+        VksWindow(const LveWindow &) = delete;
+
+        VksWindow &operator=(const LveWindow &) = delete;
+
+        bool shouldClose()
+        { return glfwWindowShouldClose(window); }
+
+        void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface);
+
+        int getWidth()
+        { return width; }
+
+        int getHeight()
+        { return height; }
+
+    private:
+        void initWindow();
+
+        const int width;
+        const int height;
+
+        std::string windowName;
+        GLFWwindow *window;
+    };
+}
